29a485167c263fc0365d5ffac6002fb803cf75dd
vinc3nzo <mangasaryan.ep@gmail.com>
2023-11-21 22:59:53 +0400

    Добавлен LSP для SQL

--- lua/core/lspconfig.lua
+++ lua/core/lspconfig.lua
@@ -7,13 +7,23 @@ local function config()
   mason_lspconfig.setup({
     ensure_installed = {
       "lua_ls",
+      "sqlls",
     },
   })
 
+  -- Lua --
   lspconfig.lua_ls.setup({})
+
+  -- Haskell --
   lspconfig.hls.setup({
     filetypes = { "haskell", "lhaskell", "cabal" },
   })
+
+  -- SQL (Postgres)
+  lspconfig.sqlls.setup({})
+
+  -- display documentation
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 end
 
 return {
