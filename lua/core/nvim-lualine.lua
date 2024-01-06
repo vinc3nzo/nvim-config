e85627d0f3a2b64430a6b7834ae3d7c08587b2da
Evgeny Mangasaryan <mangasaryan.ep@gmail.com>
2024-01-06 15:24:25 +0400

    Снизить привязку к шрифту в lualine

--- lua/core/nvim-lualine.lua
+++ lua/core/nvim-lualine.lua
@@ -5,6 +5,33 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("lualine").setup()
+    require("lualine").setup({
+      options = {
+        component_separators = {
+          left = "",
+          right = "",
+        },
+        section_separators = {
+          left = "",
+          right = "",
+        },
+        globalstatus = false,
+      },
+      sections = {
+        lualine_a = {"mode"},
+        lualine_b = {"branch", "diff"},
+        lualine_c = {"filename"},
+        -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+        -- lualine_y = {'progress'},
+        -- lualine_z = {'location'}
+        lualine_x = {
+          { "diagnostics", sources = {"nvim_lsp"}, symbols = {error = " ", warn = " ", info = " ", hint = " "} },
+          "encoding",
+          "filetype"
+        },
+        lualine_y = {"progress"},
+        lualine_z = {"location"},
+      },
+    })
   end,
 }
